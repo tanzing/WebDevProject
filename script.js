0ecb3a66957d36e670797aee02f6fdfde0ebe0be
Vasu_Prads <vasudhevan090701@gmail.com>
2022-04-15 10:00:42 +0000

    Added new page and scripts

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude +
   "<br>Longitude: " + position.coords.longitude;
 }
+
+function checkvalidD(){
+
+  var x = document.getElementById("name").value;
+  var y = document.getElementById("number").value;
+  var z = document.getElementById("date").value;
+  var usernameRegex = /^[a-zA-Z0-9]+$/;
+  var test1=usernameRegex.test(x);
+  if(test1)
+     alert("Congrts. U have registerd successfully")  
+  else
+  {
+    alert(test1)
+  }
+  
+}
